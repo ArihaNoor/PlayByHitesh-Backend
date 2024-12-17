d470855294ea2113b4674907f34969c5f8abd646
ArihaNoor <arihanoor0@gmail.com>
2024-12-17 17:01:45 +0500

    final

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -15,7 +15,7 @@ const userSchema = new mongoose.Schema(
       unique: true,
       lowercase: true,
       trim: true, //removes spaces while storing
-      index: true, //for faster search to make the fields searchable    
+      index: true, //for faster search to make the fields searchable
     },
     email: {
       type: String,
@@ -48,9 +48,16 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-userSchema.pre("save", async function(next){
-    this.password = await bcrypt.hash(this.password, 10);
-    next();
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = bcrypt.hash(this.password, 10);
+  next();
 });
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 export const User = mongoose.model("User", userSchema);
